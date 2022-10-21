dd05e36f61616688e5d58184813c5e23dd278a5e
juneyoung <jadw951623@gmail.com>
2022-10-21 18:20:26 +0900

    feat. auth 레이아웃 작업 중

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import AuthLayout from "../../components/auth/AuthLayout";
 import Button from "../../components/common/Button";
 
 const Login = () => {
   return (
-    <div>
+    <AuthLayout>
       <Button blue>로그인</Button>
-    </div>
+    </AuthLayout>
   );
 };
 
--- styles/theme.ts
+++ styles/theme.ts
@@ -11,8 +11,6 @@ export const theme: DefaultTheme = {
   },
   boxShadow: {
     normal: "0 3px 8px 0 rgb(0 0 0 / 10%)",
-    purple: "0 3px 8px 0 #d6c9ff",
-    blue: "0 3px 8px 0 #b3e2e6",
   },
 };
 
