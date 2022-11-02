491aa85e843b179f56b9596fcc2f6c909c6ce7e0
juneyoung <jadw951623@gmail.com>
2022-11-02 15:35:22 +0900

    feat. auth 페이지들 로그인 상태 시 페이지 이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import type { AppProps } from 'next/app';
+import type { AppContext, AppInitialProps, AppProps } from 'next/app';
 import Head from 'next/head';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
-import wrapper from '../src/store/configureStore';
+import wrapper, { store } from '../src/store/configureStore';
 import { GlobalStyle } from '@/styles/global-styles';
+import { userActions } from '@/src/store/reducers';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -21,5 +22,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
+MyApp.getInitialProps = async ({ Component, ctx }: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
 
 export default wrapper.withRedux(MyApp);
