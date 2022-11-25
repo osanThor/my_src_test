47f6762362ae8f5af4588a3e76ce566a222e5e11
juneyoung <jadw951623@gmail.com>
2022-11-25 14:10:35 +0900

    feat. community layout

---
+++ src/components/community/CommunityLayout.tsx
@@ -0,0 +1,97 @@
+import colors from '@/src/assets/Colors';
+import { RootState } from '@/src/store/configureStore';
+import { media } from '@/styles/theme';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+const CommunityLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const { communityDiscussion, communityNotice } = useSelector(({ local }: RootState) => ({
+    communityDiscussion: local.communityDiscussion,
+    communityNotice: local.communityNotice,
+  }));
+  return (
+    <CommunityLayoutBlock className="container">
+      <CommunityLayoutSpacer />
+      <div className="community_top">
+        <div className="community_tab">
+          <div
+            className={communityDiscussion ? 'button on' : 'button'}
+            onClick={() => router.push('/community?category=discussion')}
+          >
+            전략토론
+          </div>
+          <div
+            className={communityNotice ? 'button on' : 'button'}
+            onClick={() => router.push('/community?category=notice')}
+          >
+            공지사항
+          </div>
+        </div>
+      </div>
+      {children}
+    </CommunityLayoutBlock>
+  );
+};
+
+const CommunityLayoutBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .community_top {
+    display: flex;
+    align-items: center;
+    margin-bottom: 24px;
+    .community_tab {
+      display: flex;
+      align-items: center;
+      .button {
+        cursor: pointer;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 8px 20px;
+        border-radius: 20px;
+        background-color: ${colors.gray[1]};
+        color: ${colors.gray[5]};
+        margin-right: 1rem;
+        transition: all 0.2s;
+        &:last-child {
+          margin-right: 0;
+        }
+        &:hover {
+          background-color: ${colors.gray[2]};
+        }
+
+        &.on {
+          background-color: ${colors.blue[2]};
+          color: white;
+          &:hover {
+            background-color: ${colors.blue[1]};
+          }
+        }
+        &.error {
+          background-color: ${colors.red[2]};
+          color: white;
+          &:hover {
+            background-color: ${colors.red[1]};
+          }
+        }
+      }
+    }
+  }
+  ${media.tablet} {
+    .community_top {
+      display: none;
+    }
+  }
+`;
+const CommunityLayoutSpacer = styled.div`
+  ${media.tablet} {
+    width: 100%;
+    height: 56px;
+  }
+`;
+export default CommunityLayout;
--- src/pages/Community/CommunityIndex.tsx
+++ src/pages/Community/CommunityIndex.tsx
@@ -1,10 +1,32 @@
+import CommunityLayout from '@/src/components/community/CommunityLayout';
 import UserLayout from '@/src/components/layout/UserLayout';
-import React from 'react';
+import { RootState } from '@/src/store/configureStore';
+import { localActions } from '@/src/store/reducers';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 const CommunityIndex = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { communityDiscussion, communityNotice } = useSelector(({ local }: RootState) => ({
+    communityDiscussion: local.communityDiscussion,
+    communityNotice: local.communityNotice,
+  }));
+  useEffect(() => {
+    if (router.query.category === 'discussion') {
+      dispatch(localActions.gotoComDiscussion());
+    } else if (router.query.category === 'notice') {
+      dispatch(localActions.gotoComNotice());
+    }
+  }, [router]);
   return (
     <UserLayout>
-      <div>커뮤니티 Board</div>
+      <CommunityLayout>
+        {communityDiscussion && <div>전략토론</div>}
+        {communityNotice && <div>공지</div>}
+      </CommunityLayout>
     </UserLayout>
   );
 };
