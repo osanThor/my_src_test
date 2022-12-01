3d27f0032f76de1da6a3761501cdae83fb30186e
juneyoung <jadw951623@gmail.com>
2022-12-01 14:33:53 +0900

    feat. board mobile header

---
+++ src/components/common/header/MBoardHeader.tsx
@@ -0,0 +1,65 @@
+import { ArrowLeft, Menu7, ShareIcon } from '@/src/assets/Images';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React from 'react';
+import styled from 'styled-components';
+
+const MBoardHeader = () => {
+  const router = useRouter();
+
+  return (
+    <MBoardHeaderBlock>
+      <div className="backBtn" onClick={() => router.back()}>
+        <Image src={ArrowLeft} alt="back button" />
+      </div>
+      <div className="right_btns">
+        <div className="button">
+          <Image src={Menu7[0]} alt="collection" />
+        </div>
+        <div className="button">
+          <Image src={ShareIcon} alt="share" />
+        </div>
+      </div>
+    </MBoardHeaderBlock>
+  );
+};
+
+const MBoardHeaderBlock = styled.div`
+  width: 100%;
+  height: 50px;
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.bgColor};
+  position: fixed;
+  z-index: 997;
+
+  .backBtn {
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    span {
+      width: 24px;
+      height: 24px;
+      position: relative;
+    }
+  }
+  .right_btns {
+    display: flex;
+    align-items: center;
+    .button {
+      width: 24px;
+      height: 24px;
+      margin-left: 16px;
+      &:first-child {
+        margin-left: 0;
+      }
+    }
+  }
+`;
+
+export default MBoardHeader;
